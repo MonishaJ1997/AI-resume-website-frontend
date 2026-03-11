56818b94267f9bf469a77dfd3516570039166087
MonishaJ1997 <monishapython@vtscoders.com>
2026-03-11 17:01:49 +0530

    Update Blog.jsx

--- src/components/Blog.jsx
+++ src/components/Blog.jsx
@@ -6,7 +6,7 @@ import Navbar from "./Navbar";
 import Footer from "./Footer";
 
 function Blog() {
- const BASE_URL = "http://127.0.0.1:8000";
+ const BASE_URL = "https://ai-resume-website-backend.onrender.com";
   const [blogs, setBlogs] = useState([]);
 
   useEffect(() => {
