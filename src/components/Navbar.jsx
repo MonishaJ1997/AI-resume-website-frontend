4e2e8fb17523bcc0d9d9d12f48c36461ce2ab445
MonishaJ1997 <monishapython@vtscoders.com>
2026-03-11 17:04:09 +0530

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,7 +3,7 @@ import { NavLink, useNavigate } from "react-router-dom";
 import { useState, useEffect } from "react";
 
 function Navbar(){
- const BASE_URL = "http://127.0.0.1:8000";
+ const BASE_URL = "https://ai-resume-website-backend.onrender.com";
   const navigate = useNavigate();
 
   const [userName, setUserName] = useState("");
